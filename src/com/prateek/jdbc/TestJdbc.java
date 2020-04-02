102b85b5459d9ef64129e6acaad1fa13e159fad5
Prateek <prateek.verma0701@gmail.com>
2020-04-03 02:49:12 +0530

    Testing database connection or creating jdbc connection

---
+++ src/com/prateek/jdbc/TestJdbc.java
@@ -0,0 +1,27 @@
+package com.prateek.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=UTC";
+		String username = "root";
+		String password = "prateek@123";
+
+		try {
+
+			System.out.println("Connecting to MySql Database" + jdbcUrl);
+
+			Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+			System.out.println("Connection Successful");
+
+		} catch (Exception exception) {
+
+		}
+	}
+
+}
