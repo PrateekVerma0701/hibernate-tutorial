71860765603ce324b5a109e9c94c212fad0287b6
Prateek <prateek.verma0701@gmail.com>
2020-04-04 03:36:33 +0530

    Querying objects - simple createQuery example

---
+++ src/com/prateek/hibernate/demo/QueryStudentDemo.java
@@ -0,0 +1,39 @@
+package com.prateek.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.prateek.hibernate.demo.entity.Student;
+
+public class QueryStudentDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class).buildSessionFactory();
+
+		// create session
+		Session session = sessionFactory.getCurrentSession();
+
+		try {
+			// create a student object
+			System.out.println("Creating  a new student object...");
+			Student student = new Student("Poorva", "Verma", "poorva.verma@yahoo.com");
+
+			// start a transaction
+			session.beginTransaction();
+
+			// save the student object
+			System.out.println("Saving the student record...");
+			session.save(student);
+
+			// commit transaction
+			session.getTransaction().commit();
+		} finally {
+			sessionFactory.close();
+		}
+	}
+
+}
